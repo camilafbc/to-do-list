0c5fb294ed0160150bbaaba992cd9adb1504914b
Camila F. B. Coelho <camilafernanda914@gmail.com>
2023-09-01 17:50:40 -0300

    iniciando utilização do localstorage

--- js/script.js
+++ js/script.js
@@ -2,18 +2,34 @@ const button = document.getElementById("btn");
 const list = document.getElementById("list");
 const input_text = document.querySelector("input[name='task']");
 
-function handleAddToList(){
 
-    if(input_text.value == "") return
+
+const tarefas = []
+const minhaTarefas = localStorage.getItem('minha-to-do-list')
+if(minhaTarefas){
+    const existe = JSON.parse(minhaTarefas)
+
+existe.forEach((ex) => createTask(ex.texto, ex.check))
+}
+
+
+
+function toLocalStorage(){
+    localStorage.setItem('minha-to-do-list', JSON.stringify(tarefas))
+}
+
+
+function createTask(entrada, estado){
 
     let id = Math.floor(Math.random() * 10000);
 
     const input_check = document.createElement("input");
     input_check.type = "checkbox";
     input_check.id = `check-task-${id}`;
+    input_check.checked = estado;
 
     const task_text = document.createElement("label");
-    task_text.innerText = input_text.value;
+    task_text.innerText = entrada;
     task_text.htmlFor = `check-task-${id}`;
 
     const delete_btn = document.createElement("button");
@@ -28,15 +44,26 @@ function handleAddToList(){
     list_task.className = "list_task";
     list_task.append(input_check, task_text, edit_btn, delete_btn);
     list.appendChild(list_task);
+    if(estado){
+        list_task.classList.add("teste")
+    }
 
     delete_btn.addEventListener('click', (ev) => {
         const task_to_remove = ev.currentTarget.parentNode;
         task_to_remove.remove();
     })
 
+    
     input_check.addEventListener('change', (ev) => {
         const check = ev.currentTarget.parentNode;
+        // check.classList.toggle("teste");
+        // input_check.checked = estado;
+        const novoEstado = input_check.checked;
         check.classList.toggle("teste");
+        // console.log(check)
+        // console.log(check.id)
+        console.log(novoEstado)
+        updateTask(novoEstado, id)
     })
 
     edit_btn.addEventListener('click', (ev) => {
@@ -44,6 +71,24 @@ function handleAddToList(){
         const newText = prompt("Editar Tarefa: ", edit_task.innerText);
         edit_task.innerText = newText;
     })
+
+    tarefas.push({id: id, texto: entrada, check: estado})
+    toLocalStorage()
+}
+
+function updateTask(estado, id){
+    const tarefaAAtualizar = tarefas.find(tarefa => tarefa.id == id)
+    if(tarefaAAtualizar){
+        tarefaAAtualizar.check = estado
+        toLocalStorage()
+    }
+}
+
+function handleAddToList(){
+
+    if(input_text.value == "") return
+
+    createTask(input_text.value);
 
     input_text.value = "";
 
